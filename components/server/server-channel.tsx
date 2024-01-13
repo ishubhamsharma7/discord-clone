0f7ac383c53ee3aa04dd8ff0c116fb9c6aedd76f
shubham <shubhamsharma649@gmail.com>
2024-01-13 20:49:42 +0530

    added delete and edit channel and its api

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,6 +5,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "../action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
     channel: Channel;
@@ -20,6 +21,8 @@ const iconMap = {
 const ServerChannel = ({
     channel,server,role
 }:ServerChannelProps) => {
+    
+    const {onOpen} = useModal();
 
     const params = useParams()
     const router = useRouter()
@@ -42,10 +45,14 @@ const ServerChannel = ({
         {channel.name !== 'general' && role !== MemberRole.GUEST && (
             <div className=" ml-auto flex items-center gap-x-2">
                 <ActionTooltip label="Edit">
-                    <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"  />
+                    <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"  
+                    onClick={()=>onOpen('editChannel',{server,channel})}
+                    />
                 </ActionTooltip>
                 <ActionTooltip label="Delete">
-                    <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"  />
+                    <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" 
+                    onClick={()=>onOpen('deleteChannel',{server,channel})}
+                    />
                 </ActionTooltip>
             </div>
         )}
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,14 +1,15 @@
 // hook to control all modals in our app
 
 import { ServerWithMembersWithProfiles } from '@/types';
-import { ChannelType, Server } from '@prisma/client';
+import { Channel, ChannelType, Server } from '@prisma/client';
 import {create} from 'zustand'
 
-export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel' | 'leaveServer' | 'deleteServer';
+export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel' | 'leaveServer' | 'deleteServer' | 'deleteChannel' | 'editChannel';
 
 interface ModalData {
     server?: Server;
-    channelType?: ChannelType
+    channel?: Channel;
+    channelType?: ChannelType;
 }
 
 interface ModalStore {
