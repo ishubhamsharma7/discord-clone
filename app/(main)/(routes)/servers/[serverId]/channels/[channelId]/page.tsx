4d7eecc572e83bd0aa448533138811154649b5b6
shubham <shubhamsharma649@gmail.com>
2024-01-13 22:21:35 +0530

    added chat header and hamburger menu for mobile view

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,44 @@
-const ChannelIdPage = () => {
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps{
+    params:{
+        severId:string;
+        channelId:string
+    }
+}
+
+
+const ChannelIdPage = async({params}:ChannelIdPageProps) => {
+
+    const profile = await currentProfile();
+
+    if(!profile) return redirectToSignIn()
+
+    const channel = await db.channel.findUnique({
+        where:{
+            id:params.channelId,
+        }
+    })
+
+    const member = await db.member.findFirst({
+        where:{
+            serverId: params.severId,
+            profileId: profile.id
+        }
+    })
+
+    if(!channel || !member){
+        redirect('/')
+    }
+
     return ( 
-        <div>
-            Channel Id Page
+        <div className=" bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader name={channel?.name} type="channel" serverId={channel?.serverId}  />
+            
         </div>
      );
 }
